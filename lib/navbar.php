9ef2fc720d6881771986f5760e32f3df2975cb9a
carlos soliz <carlos.soliz.t1vl@gmail.com>
2024-08-20 20:08:13 -0300

    v6

--- lib/navbar.php
+++ lib/navbar.php
@@ -1,17 +1,19 @@
 <header>
-        <nav>
-            <ul>
-                <li class="logo"><a href="index.php"><img class="logo_header" src="./assect/logo2.png"></a></li>
-                <li class="items"><a href="index.php">Inicio</a></li>
-                <li class="items"><a href="category.php">Catalogo</a></li>
-                <?php 
-                    if (!isset($_SESSION['usuario_id'])) {
-                        echo "<li class='items'><a href='login.php'> Iniciar Sesión</a></li>";
-                    } else {
-                        echo "<li class='items'><a href='./lib/cerrar_sesion.php'>Cerrar Sesión</a></li>";
-                    }
-                ?>
-                
-            </ul>
-        </nav>
-    </header>
+    <nav>
+        <ul>
+            <li class="logo"><a href="index.php"><img class="logo_header" src="./assect/logo2.png"></a></li>
+            <li class="items"><a href="index.php">Inicio</a></li>
+            <li class="items"><a href="category.php">Catalogo</a></li>
+            <?php
+            if (!isset($_SESSION['usuario_id'])) {
+                echo "<li class='items'><a href='login.php'> Iniciar Sesión</a></li>";
+                echo "<script>var login = 'false'</script>";
+            } else {
+                echo "<li class='items'><a href='./lib/cerrar_sesion.php'>Cerrar Sesión</a></li>";
+                echo "<script>var login = 'true'</script>";
+            }
+            ?>
+
+        </ul>
+    </nav>
+</header>
